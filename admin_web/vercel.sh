38ad169d81107bccbc895c1b48e0be043d5d64b5
Rahim-228 <24228@supnum.mr>
2026-06-13 03:22:48 +0000

    Fix Vercel Flutter build

--- admin_web/vercel.sh
+++ admin_web/vercel.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+set -e
 
 git clone https://github.com/flutter/flutter.git -b stable
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
+
 flutter config --enable-web
+flutter precache --web
 flutter pub get
-flutter build web
+flutter build web --release
